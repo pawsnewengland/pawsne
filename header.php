8f09fa2d32940c9c27c30a0d25b0d0a10c1132cb
Chris Ferdinandi <hello@gomakethings.com>
2013-04-01 08:35:17 -0400

    Fixed Special Needs content glitch

--- header.php
+++ header.php
@@ -24,7 +24,7 @@
 
 
 	    <!-- Stylesheet -->
-	    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/style.min.03272013.css">
+	    <link rel="stylesheet" href="<?php bloginfo('stylesheet_directory'); ?>/style.min.04012013.css">
 
         <!-- HTML5 Shim for IE 6-8 -->
 	    <!--[if lt IE 9]>    
