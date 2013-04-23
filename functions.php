cff91efbdf8d179ac62707d17690ca07b3bc59b1
Chris Ferdinandi <hello@gomakethings.com>
2013-04-23 13:13:48 -0400

    Fixed Disable-Inline-Styles.php
    Disable-Inline-Styles.php was removing needed <span> elements from
    pages. Fixed to only remove inline styles on blog posts.

--- functions.php
+++ functions.php
@@ -176,15 +176,19 @@ update_option('image_default_link_type','none');
 add_filter( 'the_content', 'clean_post_content' );
 function clean_post_content($content) {
 
-    // Remove inline styling
-    $content = preg_replace('/(<[^>]+) style=".*?"/i', '$1', $content);
+    if ( is_single() || is_home() ) {
 
-    // Remove font tag
-    $content = preg_replace('/<font[^>]+>/', '', $content);
+        // Remove inline styling
+        $content = preg_replace('/(<[^>]+) style=".*?"/i', '$1', $content);
 
-    // Remove empty tags
-    $post_cleaners = array('<p></p>' => '', '<p> </p>' => '', '<p>&nbsp;</p>' => '', '<span></span>' => '', '<span> </span>' => '', '<span>&nbsp;</span>' => '', '<span>' => '', '</span>' => '', '<font>' => '', '</font>' => '');
-    $content = strtr($content, $post_cleaners);
+        // Remove font tag
+        $content = preg_replace('/<font[^>]+>/', '', $content);
+
+        // Remove empty tags
+        $post_cleaners = array('<p></p>' => '', '<p> </p>' => '', '<p>&nbsp;</p>' => '', '<span></span>' => '', '<span> </span>' => '', '<span>&nbsp;</span>' => '', '<span>' => '', '</span>' => '', '<font>' => '', '</font>' => '');
+        $content = strtr($content, $post_cleaners);
+
+    }
 
     return $content;
 }
