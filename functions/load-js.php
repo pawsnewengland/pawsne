933491a9d2873f39914d2822117f51dc2a2644b8
Chris Ferdinandi <hello@gomakethings.com>
2013-11-22 13:55:05 -0500

    Updated script names to load

--- functions/load-js.php
+++ functions/load-js.php
@@ -2,20 +2,20 @@
 
 /* ======================================================================
 
-    Load JavaScript v1.0
-    Load theme JavaScript file in WordPress.
-    Learn more: http://codex.wordpress.org/Function_Reference/wp_register_script
+	Load JavaScript v1.0
+	Load theme JavaScript file in WordPress.
+	Learn more: http://codex.wordpress.org/Function_Reference/wp_register_script
 
  * ====================================================================== */
 
 // Load theme JS
 function load_theme_js() {
 	// Feature Test (in header)
-	wp_register_script('feature-test', get_template_directory_uri() . '/js/feature-test.min.11202013.js', false, null, false);
+	wp_register_script('feature-test', get_template_directory_uri() . '/js/feature-test.min.11222013.js', false, null, false);
 	wp_enqueue_script('feature-test');
 
 	// Theme scripts (in footer)
-	wp_register_script('pne-js', get_template_directory_uri() . '/js/pne.min.11202013.js', false, null, true);
+	wp_register_script('pne-js', get_template_directory_uri() . '/js/pne.min.11222013.js', false, null, true);
 	wp_enqueue_script('pne-js');
 }
 add_action('wp_enqueue_scripts', 'load_theme_js');
