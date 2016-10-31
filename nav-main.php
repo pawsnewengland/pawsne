e5fba4bb2ff1762806ab1fbfda64b84c204dcec0
cferdinandi <chris@gomakethings.com>
2016-10-31 09:01:08 -0400

    v6.19.0 Added cart count for WooCommerce

--- nav-main.php
+++ nav-main.php
@@ -7,8 +7,12 @@
  */
 
 ?>
+<?php
+	// Get number of items in cart
+	$cart_count = class_exists( 'WooCommerce' ) ? WC()->cart->get_cart_contents_count() : 0;
+?>
 
-<nav class="nav-wrap-navbar nav-collapse <?php if ( !keel_has_hero() ) { echo 'margin-bottom'; } ?>  no-js-drop">
+<nav class="nav-wrap-navbar nav-collapse <?php if ( !keel_has_hero() && $cart_count === 0 ) { echo 'margin-bottom'; } ?>  no-js-drop">
 	<div class="container container-large">
 		<a class="logo-navbar" href="<?php echo site_url(); ?>/">
 			<?php
@@ -42,3 +46,13 @@
 		<?php endif; ?>
 	</div>
 </nav>
+<?php if ( $cart_count !== 0 ) : ?>
+	<nav class="bg-muted padding-top-small padding-bottom-small <?php if ( !keel_has_hero() ) { echo 'margin-bottom'; } ?>">
+		<div class="container container-large">
+			<a class="float-right" href="<?php echo wc_get_cart_url(); ?>">
+				<svg xmlns="http://www.w3.org/2000/svg" class="icon" viewBox="0 0 17 17"><path d="M6.375 15.406c0 .88-.714 1.594-1.594 1.594s-1.593-.714-1.593-1.594c0-.88.714-1.594 1.594-1.594s1.595.714 1.595 1.594zM17 15.406c0 .88-.714 1.594-1.594 1.594s-1.594-.714-1.594-1.594c0-.88.714-1.594 1.594-1.594S17 14.526 17 15.406zM17 8.5V2.125H4.25c0-.587-.476-1.063-1.063-1.063H0v1.063h2.124l.798 6.84c-.486.39-.798.99-.798 1.66 0 1.174.95 2.125 2.125 2.125H17v-1.063H4.25c-.588 0-1.064-.476-1.064-1.063v-.01l13.812-2.115z"/></svg>
+				<?php _e( 'Your Cart', 'keel' ) ?> (<?php echo esc_html( $cart_count ); ?>)
+			</a>
+		</div>
+	</nav>
+<?php endif; ?>
